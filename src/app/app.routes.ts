c8f6273202a8e38faf84e10b49708a86785dcbb3
Kevin Girelli <kevingirelli.gg@gmail.com>
2024-05-12 14:25:23 -0300

    Add Home Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { SomaComponent } from './components/soma/soma.component';
 import { ContaLetraComponent } from './components/conta-letra/conta-letra.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: "letra",
     component: ContaLetraComponent
